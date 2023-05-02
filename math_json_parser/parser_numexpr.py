e0ce531bd3f7a47ce05983d72571dbd441e8555c
Shengheng Yan <timbrist@outlook.com>
2023-05-02 16:02:26 +0300

    unify 2 expressions

--- math_json_parser/parser_numexpr.py
+++ math_json_parser/parser_numexpr.py
@@ -14,40 +14,27 @@ class parser_numexpr:
         "Multiply":              lambda a,b: '(%s *  %s)' % (a, b),
         "Divide":                lambda a,b: '(%s /  %s)' % (a, b),
         "Power":                 lambda a,b: '(%s ** %s)' % (a, b),
-        "Arctan2":               lambda a,b: 'arctan2(%s,%s)' % (a,b), #math.atan2(x,y)
-    }
-    function_map = {
+        "Arctan2":               lambda a,b: 'arctan2(%s,%s)' % (a,b), #math.atan2(x,y) 
         "Sqrt":                  lambda a: 'sqrt(%s)' % (a), 
         "Expm1":                 lambda a: 'expm1(%s)' % (a),
-        #Rounding
-        "Abs":                   lambda a: 'abs(%s)' % (a),
-
-        #Trigonometry
+        "Abs":                   lambda a: 'abs(%s)' % (a), 
         "Sin":                   lambda a: 'sin(%s)' % (a),
         "Cos":                   lambda a: 'cos(%s)' % (a),
         "Tan":                   lambda a: 'tan(%s)' % (a),
         "Arcsin":                lambda a: 'arcsin(%s)' % (a),#∀ x ∊ [ − 1 , 1 ] 
         "Arccos":                lambda a: 'arccos(%s)' % (a),
-        "Arctan":                lambda a: 'arctan(%s)' % (a), #∀ x ∊ ( − 1 , 1 )
-       
+        "Arctan":                lambda a: 'arctan(%s)' % (a), #∀ x ∊ ( − 1 , 1 ) 
         "Sinh":                  lambda a: 'sinh(%s)' % (a),
         "Cosh":                  lambda a: 'cosh(%s)' % (a),
         "Tanh":                  lambda a: 'tanh(%s)' % (a),
         "Arcsinh":               lambda a: 'arcsinh(%s)' % (a),
         "Arccosh":               lambda a: 'arccosh(%s)' % (a),#∀x >= 1 ,
-        "Arctanh":               lambda a: 'arctanh(%s)' % (a),
-
-        #Transcendental Functions
+        "Arctanh":               lambda a: 'arctanh(%s)' % (a), 
         "Exp":                   lambda a: 'exp(%s)' % (a),
         "Ln":                    lambda a: 'log(%s)' % (a),
         "Lg":                    lambda a: 'log10(%s)' % (a),
         "LogOnePlus":            lambda a: '10g1p(%s)' % (a),
     }
-    expr = {
-        "Operator": operator_map,
-        "Function": function_map
-    }
-
     def __init__(self) -> None:
         self.numpy_expr = '' 
     def set_numpy_expr(self, __expr: str) -> None:
@@ -60,7 +47,7 @@ class parser_numexpr:
             operator = textlist[0]
             parameter_len =  len(textlist[1:]) 
             if parameter_len == 1:
-                return self.function_map[operator](self.parser2(textlist[1]))
+                return self.operator_map[operator](self.parser2(textlist[1]))
             elif parameter_len == 2:
                 return self.operator_map[operator](self.parser2(textlist[1]), self.parser2(textlist[2]))
             else:
--- math_json_parser/parser_polars.py
+++ math_json_parser/parser_polars.py
@@ -1,89 +1,45 @@
 import polars as pl
 
+
+#polars expression:
+#https://pola-rs.github.io/polars/py-polars/html/reference/expressions/computation.html
 class parser_polars:
-    #Relational Operators
-    def equal(x,y):
-        return x == y
-    def greater(x,y):
-        return x > y
-    def greater_equal(x,y):
-        return x>=y
-    def less(x,y):
-        return x < y
-    def less_equal(x,y):
-        return x <= y
-    def not_equal(x,y):
-        return x!=y
-    #Functions
-    def add(a,b):
-        return a + b
-    def substract(a,b):
-        return a - b
-    def negate(a):
-        return 0-a
-    def multiply(a,b):
-        return a * b
-    def divide(a,b):
-        try:
-            return a/b
-        except:
-            print("exception: try to divide 0")
-    def power(a,b):
-        return a**b
-    def root(x,n):
-        return x**(1/n)
-
     operator_map = {
+        "Equal":                 lambda a,b: a == b,  
+        "Greater":               lambda a,b: a >  b,
+        "GreaterEqual":          lambda a,b: a >= b,
+        "Less":                  lambda a,b: a <  b,
+        "LessEqual":             lambda a,b: a <= b,
+        "NotEqual":              lambda a,b: a != b,
+        "Add":                   lambda a,b: a +  b,
+        "Substract":             lambda a,b: a -  b,
+        "Negate":                lambda a,b: a -  b,
+        "Multiply":              lambda a,b: a *  b,
+        "Divide":                lambda a,b: a /  b,
+        "Power":                 lambda a,b: a ** b,
 
-        #Relational Operators
-        "Equal":equal,
-        "Greater":greater,
-        "GreaterEqual":greater_equal,
-        "Less":less,
-        "LessEqual":less_equal,
-        "NotEqual":not_equal,
-
-        #Functions
-        "Add":add,
-        "Substract":substract,
-        "Negate":negate,
-        "Multiply":multiply,
-        "Divide":divide,
-        "Power":power,
-        "Root":root,
-        "Sqrt":lambda x: x**(1/2),
-        "Square":lambda x: x**2,
-
-        #polars expression:
-        #https://pola-rs.github.io/polars/py-polars/html/reference/expressions/computation.html
-
-        "Abs": lambda x: pl.Expr.abs(x),
-        "Approch_Unique": lambda x: pl.Expr.approx_unique(x),
-        "Arccos": lambda x: pl.Expr.arccos(x),
-        "Arccosh": lambda x: pl.Expr.arccosh(x),
-        "Arcsin": lambda x: pl.Expr.arcsin(x),
-        "Arcsinh": lambda x: pl.Expr.arcsinh(x),
-        "Arctan": lambda x: pl.Expr.arctan(x),
-        "Arctanh": lambda x: pl.Expr.arctanh(x),
-        "Arg_Unique": lambda x: pl.Expr.arg_unique(x),
-        "Cos":lambda x : pl.Expr.cos(x),
-        "Cosh":lambda x: pl.Expr.cosh(x),
-
-        "Exp":lambda x: pl.Expr.exp(x),
-        "Ln": lambda x: pl.Expr.log(x),
-        "Log": lambda x, base: pl.Expr.log(x,base),
-        "Lg": lambda x: pl.Expr.log10(x),
-        "LogOnePlus": lambda x: pl.Expr.log1p(x),
-
-
-
-        "Ceil": lambda x: pl.Expr.ceil(x),
-        "Floor": lambda x: pl.Expr.floor(x),
-
-        "Sin": lambda x: pl.Expr.sin(x),
-        "Sinh": lambda x: pl.Expr.sinh(x),
-        "Tan":lambda x : pl.Expr.tan(x),
-        "Tanh":lambda x: pl.Expr.tanh(x),
+        "Abs":                    lambda x: pl.Expr.abs(x),
+        "Approch_Unique":         lambda x: pl.Expr.approx_unique(x),
+        "Arccos":                 lambda x: pl.Expr.arccos(x),
+        "Arccosh":                lambda x: pl.Expr.arccosh(x),
+        "Arcsin":                 lambda x: pl.Expr.arcsin(x),
+        "Arcsinh":                lambda x: pl.Expr.arcsinh(x),
+        "Arctan":                 lambda x: pl.Expr.arctan(x),
+        "Arctanh":                lambda x: pl.Expr.arctanh(x),
+        "Arg_Unique":             lambda x: pl.Expr.arg_unique(x),
+        "Cos":                    lambda x: pl.Expr.cos(x),
+        "Cosh":                   lambda x: pl.Expr.cosh(x),
+        "Exp":                    lambda x: pl.Expr.exp(x),
+        "Ln":                     lambda x: pl.Expr.log(x),
+        "Log":                    lambda x, base: pl.Expr.log(x,base),
+        "Lg":                     lambda x: pl.Expr.log10(x),
+        "LogOnePlus":             lambda x: pl.Expr.log1p(x),
+        "Ceil":                   lambda x: pl.Expr.ceil(x),
+        "Floor":                  lambda x: pl.Expr.floor(x),
+        "Sin":                    lambda x: pl.Expr.sin(x),
+        "Sinh":                   lambda x: pl.Expr.sinh(x),
+        "Tan":                    lambda x: pl.Expr.tan(x),
+        "Tanh":                   lambda x: pl.Expr.tanh(x),
     }
     def parser(self,textlist):
         if isinstance(textlist, list):
